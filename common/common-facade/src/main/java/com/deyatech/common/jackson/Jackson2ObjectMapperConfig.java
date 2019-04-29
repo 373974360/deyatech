16e5a429e7db5d2b44515d0ea69da2402ce0e490
李苛 <373974360@qq.com>
2019-04-29 15:54:11 +0800

    增加jackson解析json配置

---
+++ common/common-facade/src/main/java/com/deyatech/common/jackson/Jackson2ObjectMapperConfig.java
@@ -0,0 +1,33 @@
+package com.deyatech.common.jackson;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.stereotype.Component;
+
+import java.util.TimeZone;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author: lee.
+ * @since: 2019/4/29 15:52
+ */
+@Component
+public class Jackson2ObjectMapperConfig {
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        return Jackson2ObjectMapperBuilder.json()
+                .serializationInclusion(JsonInclude.Include.NON_NULL)
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .timeZone(TimeZone.getTimeZone("Asia/Shanghai"))
+                .build();
+    }
+}
